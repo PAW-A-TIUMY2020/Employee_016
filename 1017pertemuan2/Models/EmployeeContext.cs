2b3bdc3f34311698917a7c8f6182f8706e8d0d09
Miftah-Maritah <52687746+Miftah-Maritah@users.noreply.github.com>
2020-10-19 21:32:35 +0700

    Add project files.

---
+++ 1017pertemuan2/Models/EmployeeContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace 1017pertemuan2.Models
+{
+    public class EmployeeContext
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext(DbContextOptions<EmployeeContext> options) :
+       base(options)
+        { }
+        public DbSet<Employees> Employees { get; set; }
+    }
+}
+}
---
+++ 1017pertemuan2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _1017pertemuan2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
